ff30cb0c1393b70972e9749b150ebbffd72c362b
Mike Wrather <mike.wrather@gmail.com>
2013-02-03 23:35:47 -0800

    just another commit

---
+++ application/messages/models/user/_external.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: mike
+ * Date: 2/2/13
+ * Time: 2:55 PM
+ */
+
+return array(
+	'password' => array(
+		'not_empty' => 'Please choose a password',
+	),
+	'password_confirm' => array(
+		'matches' => 'The password fields did not match.',
+	),
+);
