b28b108093a4f2ebca4f0df842a378a2ffc0effd
U-buttmuncher\mike <mike@buttmuncher.(none)>
2013-02-08 14:42:27 -0800

    Moved some stuff around

---
+++ application/classes/controller/Home.php
@@ -0,0 +1,17 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * User: mike
+ * Date: 2/8/13
+ * Time: 2:35 PM
+ */
+
+class Controller_Home extends Controller_Template
+{
+
+	public function action_index()
+	{
+
+	}
+
+}
