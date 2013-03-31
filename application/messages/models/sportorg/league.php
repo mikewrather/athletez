ec7580a163478b1b5879a9a5d1f93a2f7846f0c3
jeffrey <ws315c@gmail.com>
2013-03-31 23:50:17 +0800

    add custom validation message ,ref ticket #226

---
+++ application/messages/models/sportorg/league.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-3-31
+ * Time: 下午11:36
+ * To change this template use File | Settings | File Templates.
+ */
+
+	return array(
+		'sections_id' => array(
+			'not_equals' => ':field can\'t be null',
+		),
+		'states_id' => array(
+			'not_equals' => ':field can\'t be null',
+		)
+	);
