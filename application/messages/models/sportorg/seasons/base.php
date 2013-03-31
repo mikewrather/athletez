d9ec2688bcae9ca57e8311e828911ac4e62b6b22
jeffrey <ws315c@gmail.com>
2013-03-31 23:09:10 +0800

    add custom validation message for season section, ref ticket #236

---
+++ application/messages/models/sportorg/seasons/base.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-3-31
+ * Time: 下午10:47
+ * To change this template use File | Settings | File Templates.
+ */
+
+	return array(
+		'season_profiles_id' => array(
+			'not_equals' => ':field can\'t be null',
+		)
+	);
