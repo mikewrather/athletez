f62cc438d09add64bab749f01cc811b7578ffe2a
jeffrey <ws315c@gmail.com>
2013-04-02 21:52:08 +0800

    add custom error message

---
+++ application/messages/models/location/county.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-2
+ * Time: 下午9:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+	return array(
+		'states_id' => array(
+			'not_equals' => ':field can\'t be null',
+		)
+	);
