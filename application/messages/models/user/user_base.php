7fefd9cc40c3493a99a9e6c70b3e14d71bc87122
jeffrey <ws315c@gmail.com>
2013-04-05 00:07:43 +0800

    add save user section validate message ref #333

---
+++ application/messages/models/user/user_base.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-4
+ * Time: 下午10:04
+ * To change this template use File | Settings | File Templates.
+ */
+
+	return array(
+		'username' => array(
+			'not_empty' => 'You must provide a username.',
+			'min_length' => 'The username must be at least :param2 characters long.',
+			'max_length' => 'The username must be less than :param2 characters long.',
+			'username_available' => 'This username is already in use.',
+		),
+		'email' => array(
+			'not_empty' => 'You must enter an email address',
+			'email' =>	'Please enter valid email address',
+			'unique_email' => 'This email address is already in use.',
+		)
+	);
