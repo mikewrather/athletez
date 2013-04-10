b2b6e36ea739a04d7f39d279efae5c6a3430f9c8
jeffrey <ws315c@gmail.com>
2013-04-10 23:39:54 +0800

    complete tag rules() section. ref #356

--- application/classes/Model/Site/Tag.php
+++ application/classes/Model/Site/Tag.php
@@ -21,6 +21,34 @@ class Model_Site_Tag extends Model_Site_Entdir
 		)
 	);
 
+	public function rules(){
+
+		return array
+		(
+			// subject_enttypes_id (int)
+			'subject_enttypes_id'=>array(
+				array('not_empty'),
+				array('digit'),
+			),
+
+			// subject_id (int)
+			'subject_id'=>array(
+				array('not_empty'),
+				array('digit'),
+				array('subject_id_exist',array( ':validation', 'subject_enttypes_id', 'subject_id'))
+			),
+
+			// users_id (int)
+			//TODO, add by jeffrey, this user id validation need developer do manually.
+			/*
+			'users_id'=>array(
+				array('not_empty'),
+				array('digit'),
+			),
+			*/
+		);
+	}
+
 	public function __construct($id=NULL)
 	{
 		parent::__construct($id);
