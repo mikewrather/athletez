448dd1ff1d2bbbe089dd9a0f28fc0f3e47f2a69e
jeffrey <ws315c@gmail.com>
2013-04-13 07:56:52 +0800

    add resum data message

---
+++ application/messages/models/user/resume/user_resume_data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-13
+ * Time: 上午7:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+	return array(
+		'resume_data_type' => array(
+			'in_array' => "Resume data type only accept 'number','string','time'"
+		),
+	);
