a78e3e9142c1c322db3678b76b6e08be617231bf
jeffrey <ws315c@gmail.com>
2013-04-13 10:35:09 +0800

    add custom message file

---
+++ application/messages/models/sportorg/games/sportorg_games_base.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-13
+ * Time: 上午10:22
+ * To change this template use File | Settings | File Templates.
+ */
+	$e = Kohana::$config->load('error_messages');
+	return array(
+
+	);
