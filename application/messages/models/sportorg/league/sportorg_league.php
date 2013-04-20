ff0251a02263c0d8a8a5295f4feb05f496a41667
jeffrey <ws315c@gmail.com>
2013-04-20 11:44:45 +0800

    add validate message file

---
+++ application/messages/models/sportorg/league/sportorg_league.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-20
+ * Time: 上午10:15
+ * To change this template use File | Settings | File Templates.
+ */
+
+	$e = Kohana::$config->load('error_messages');
+	return array(
+
+	);
