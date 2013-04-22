89316a3bed54368c0d698845d7388393877c2ed7
jeffrey <ws315c@gmail.com>
2013-04-22 22:32:05 +0800

    add validate message file

---
+++ application/messages/models/user/resume/data/user_resume_data_group.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-22
+ * Time: 下午10:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+	return array(
+
+	);
