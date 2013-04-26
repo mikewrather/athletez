31218dead8cc8995ea29c4bb37362892f6fd5014
jeffrey <ws315c@gmail.com>
2013-04-26 19:47:37 +0800

    add message file

---
+++ application/messages/models/sportorg/complevel/sportorg_complevel_profile.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-26
+ * Time: 下午7:10
+ * To change this template use File | Settings | File Templates.
+ */
+
+	return array(
+
+	);
