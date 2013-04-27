83dbb78d2be7208477a8e7d3e11f6a6548317412
jeffrey <ws315c@gmail.com>
2013-04-27 20:44:38 +0800

    Add all policies

---
+++ application/classes/Policy/Profilecontent.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Policy_ProfileContent extends Policy
+{
+
+	public function execute(Model_ACL_User $user, array $extra = NULL)
+	{
+		//TODO
+		return true;
+	}
+}
---
+++ application/classes/Policy/Recruitercontent.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Policy_RecruiterContent extends Policy
+{
+
+	public function execute(Model_ACL_User $user, array $extra = NULL)
+	{
+		//TODO
+		return true;
+	}
+}
