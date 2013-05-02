cb285a580c3358fcb540cd4fc57f50b7fd9343e6
jeffrey <ws315c@gmail.com>
2013-05-02 20:26:31 +0800

    complete add all owner(),is_owner()  to all sections

--- application/classes/Model/User/Resume/Data/Vals.php
+++ application/classes/Model/User/Resume/Data/Vals.php
@@ -136,4 +136,19 @@ class Model_User_Resume_Data_Vals extends ORM
             return $e;
         }   
 	}
+
+	public function owner(){
+		if(!$this->id){
+			return "";
+		}
+		return intval($this->users_id);
+	}
+
+	public function is_owner($user){
+		if (is_object($user)){
+			return invtal($user->id) == $this->owner();
+		}else{
+			return invtal($user) == $this->owner();
+		}
+	}
 }
