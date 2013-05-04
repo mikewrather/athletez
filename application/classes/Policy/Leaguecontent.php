5456ad542afcedb0ea8bbfebba5af56160cec803
jeffrey <ws315c@gmail.com>
2013-05-04 20:29:49 +0800

    Complete ticket #457

--- application/classes/Policy/Leaguecontent.php
+++ application/classes/Policy/Leaguecontent.php
@@ -5,7 +5,38 @@ class Policy_LeagueContent extends Policy
 
 	public function execute(Model_ACL_User $user, array $extra = NULL)
 	{
-		//TODO
+		$config = Kohana::$config->load('sysconfig');
+		$roles = $config->get('role_name');
+		$has_admin = $user->has('roles', ORM::factory('Role', array('name' => $roles['admin'])));
+		$has_league_admin = $user->has('roles', ORM::factory('Role', array('name' => $roles['league_admin'])));
+		$has_moderator = $user->has('roles', ORM::factory('Role', array('name' => $roles['moderator'])));
+		$have_permission = false;
+		if(isset($extra["action"]))
+		{
+			switch($extra["action"])
+			{
+				case 'read':
+					break;
+				case 'create':
+					break;
+				case 'modify':
+					if($has_admin || $has_league_admin || $has_moderator){
+						$have_permission = true;
+					}
+					return $have_permission;
+					break;
+				case 'delete':
+					if($has_admin){
+						$have_permission = true;
+					}
+					return $have_permission;
+					break;
+				default;
+					return false;
+					break;
+			}
+		}
+		//default no policy check
 		return true;
 	}
 }
