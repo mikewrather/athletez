0fb8638ac2822b8360f9b8a940bf05fa510c3fed
jeffrey <ws315c@gmail.com>
2013-05-04 20:16:32 +0800

    Complete ticket #453

--- application/classes/Policy/Profilecontent.php
+++ application/classes/Policy/Profilecontent.php
@@ -5,7 +5,48 @@ class Policy_ProfileContent extends Policy
 
 	public function execute(Model_ACL_User $user, array $extra = NULL)
 	{
-		//TODO
+		$config = Kohana::$config->load('sysconfig');
+		$roles = $config->get('role_name');
+		$has_admin = $user->has('roles', ORM::factory('Role', array('name' => $roles['admin'])));
+		//$has_moderator = $user->has('roles', ORM::factory('Role', array('name' => $roles['moderator'])));
+		$have_permission = false;
+		$is_owner = false;
+		if(isset($extra["entity"]))
+		{
+			$entity = $extra["entity"];
+			$is_owner = $entity->is_owner($user);
+		}
+
+		if(isset($extra["action"]))
+		{
+			switch($extra["action"])
+			{
+				case 'read':
+					break;
+				case 'create':
+					if($has_admin || $is_owner){
+						$have_permission = true;
+					}
+					return $have_permission;
+					break;
+				case 'modify':
+					if($has_admin || $is_owner){
+						$have_permission = true;
+					}
+					return $have_permission;
+					break;
+				case 'delete':
+					if($has_admin || $is_owner){
+						$have_permission = true;
+					}
+					return $have_permission;
+					break;
+				default;
+					return false;
+					break;
+			}
+		}
+		//default no policy check
 		return true;
 	}
 }
