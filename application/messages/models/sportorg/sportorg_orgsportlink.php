8cf76e4d51ca0c736caa03df8ca9b01f9dc90c80
jeffrey <ws315c@gmail.com>
2013-05-05 11:48:11 +0800

    add custom error message file

---
+++ application/messages/models/sportorg/sportorg_orgsportlink.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-4-2
+ * Time: 下午9:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+$e = Kohana::$config->load('error_messages');
+
+return array(
+
+);
