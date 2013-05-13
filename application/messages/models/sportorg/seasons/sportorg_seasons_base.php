f362facdbacbea743583b84943356921c32123c8
jeffrey <ws315c@gmail.com>
2013-05-13 22:52:18 +0800

    message file

---
+++ application/messages/models/sportorg/seasons/sportorg_seasons_base.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-5-13
+ * Time: 下午10:50
+ * To change this template use File | Settings | File Templates.
+ */
+
+	$e = Kohana::$config->load('error_messages');
+	return array(
+
+	);
