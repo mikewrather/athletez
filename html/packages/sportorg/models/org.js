f6c752eec6211f901ac277f6f0942de487943bc9
macheng <msctiger>
2013-05-18 00:28:15 +0800

    change orgs

--- html/packages/sportorg/models/org.js
+++ html/packages/sportorg/models/org.js
@@ -10,39 +10,6 @@ define(["facade", "models/base"], function (facade, BaseModel) {
 
     SportorgOrgModel = BaseModel.extend({  
 
-        defaults: _.extend({}, (new BaseModel).attributes, {
-            
-            "payload": {
-                "id": 0,
-                "name": null,
-                "sport": null,
-                "leagues": null,
-                "division": null,
-                "season_profile": null,
-                "complevel_profile": null,
-                "location": null,
-                "teams": [{
-                    "id": 0,
-                    "complevel": null,
-                    "season": null,
-                    "year": null,
-                    "mascot": null,
-                    "unique_ident": null,
-                    "statvals": [{
-                        "statval": null,
-                        "statdate": null,
-                        "statteam": null,
-                        "stat_context": null
-                    }]
-                }
-                ]
-            },
-            "desc": "Org information",
-            "exec_data": {
-                "exec_time": 0,
-                "exec_error": false
-            }
-        })
         
     });
 
