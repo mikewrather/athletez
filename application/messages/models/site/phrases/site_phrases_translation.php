8c638535b6eb92dd5310d0c8cf345e3940560f3f
jeffrey <ws315c@gmail.com>
2013-05-29 23:41:38 +0800

    add message file

---
+++ application/messages/models/site/phrases/site_phrases_translation.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Jeffrey
+ * Date: 13-5-29
+ * Time: 下午8:03
+ * To change this template use File | Settings | File Templates.
+ */
+
+return array();
