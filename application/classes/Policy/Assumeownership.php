92458f9209304b83208dc0df2c317e8237f9f3ac
jeffrey <ws315c@gmail.com>
2013-07-03 00:28:21 +0800

    implement execute

--- application/classes/Policy/Assumeownership.php
+++ application/classes/Policy/Assumeownership.php
@@ -4,9 +4,23 @@
 	{
 		public function execute(Model_ACL_User $user, array $extra = NULL)
 		{
-//			if($user->has('roles','admin'))
-//				return true;
-//			return false;
+			//$extra["owner"] should be users_id for edit/delete or create for whom
+			$config = Kohana::$config->load('sysconfig');
+			$roles = $config->get('role_name');
+			$has_admin = $user->has('roles', ORM::factory('Role', array('name' => $roles['admin'])));
+			if(isset($extra["owner"])){
+				if ($this->is_owner($user, $extra["owner"]) || $has_admin){
+					return true;
+				}
+			}else{
+				//no matter if have extra param,admin still can do it
+				if ($has_admin)
+				return true;
+			}
+			return false;
 		}
 
+		private function is_owner($user, $owner_id){
+			return invtal($user->id) == inval($owner_id);
+		}
 	}
