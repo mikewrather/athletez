291c09040ad1e5e8aa89b14df160893d702b3de6
Mike Wrather <mike.wrather@gmail.com>
2013-07-22 02:18:05 -0700

    add resume_data_id

--- application/classes/Model/User/Base.php
+++ application/classes/Model/User/Base.php
@@ -487,6 +487,7 @@ class Model_User_Base extends Model_Auth_User implements Model_ACL_User
 							"type" => $data->resume_data_type,
 							"val" => $val->loaded() ? $val->user_value : false,
 							"id" => $val->loaded() ? $val->id : 0,
+							"resume_data_id" => $data->id,
 						);
 					}
 				}
