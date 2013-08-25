e6ac130d6b18abcef1984a9ad230b13b32422d9f
yashpalwadhwa15 <yashpalwadhwa15@gmail.com>
2013-08-25 22:51:35 +0530

    Add Game Pop Up Functionality

---
+++ html/packages/sportorg/collections/sports_listall.js
@@ -0,0 +1,24 @@
+// Sporta Sports Data
+// ----------
+
+// Package Sports
+// Requires define
+// Returns {SportsList} constructor
+
+define(['facade', 'collections', 'sportorg/collections/sports', 'utils'
+], 
+function(facade, collections, SportsOrgCollection, utils) {
+
+    var SportsList,
+        BaseCollection = collections.BaseCollection,
+        _ = facade._,
+        Channel = utils.lib.Channel;
+
+    SportsList = SportsOrgCollection.extend({
+    	url: function(){
+                    return '/api/sport/listall/';
+      },
+    });
+
+    return SportsList;
+});
---
+++ html/usercontrols/addgame/collections/teams.js
@@ -0,0 +1,32 @@
+// TeamsData
+// ----------
+
+// Package Sports
+// Requires define
+// Returns {TeamsList} constructor
+define(['facade', 'collections', 'sportorg/collections/teams', 'utils'
+],  
+function(facade, collections, SportsOrgCollection, utils) {
+
+    var List,
+        BaseCollection = collections.BaseCollection,
+        _ = facade._,
+        Channel = utils.lib.Channel;
+
+    List = SportsOrgCollection.extend({
+    	url: function(){
+                var params = '';
+				if(this.team_name)
+					params += 'team_name='+ this.team_name;
+				if(this.states_id)
+					params += '&states_id=' + this.states_id;	
+				if(this.city_id)
+					params += '&city_id=' + this.city_id;	
+
+
+                    return '/api/team/search/?'+ params;
+      },
+    });
+
+    return List;
+});
