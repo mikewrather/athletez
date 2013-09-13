07281ca84c6d16f4c70d40d22c67bc9993e622d9
prasobh <prasobh.e@hotmail.com>
2013-09-14 01:22:45 +0530

    registration

---
+++ html/pages/signup/models/registerbasic.js
@@ -0,0 +1,18 @@
+define( ["models/base"], function (BaseModel) {
+
+
+	var MainModel;
+
+    MainModel = BaseModel.extend({
+
+        defaults: {
+                        
+        }
+       
+        
+    });
+
+    return MainModel;
+
+});
+
