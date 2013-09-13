686e2f5a69b2f79817de879319b8916ea5ec8185
sanjaykumar20 <sanjaykumar20@gmail.com>
2013-09-13 17:49:03 +0530

    hoge page

--- html/pages/home/views/menu.js
+++ html/pages/home/views/menu.js
@@ -15,7 +15,8 @@ define(
 					"click .searchBtn" : "changeBaseUrl",
 					"click .dropdown-menu > li > a" : "select",
 					"click .dd" : "doNothing",
-					"click .menu" : "toggle",
+					//"click .menu" : "toggle",
+					"click .menu-link-h" : "showMenuDropdown"
 				},
 
 				template : menuTemplate,
@@ -39,6 +40,26 @@ define(
 				doNothing : function(e) {
 					e.preventDefault();
 					e.stopPropagation();
+				},
+				
+				hideAllDropdowns: function() {
+					var _self = this;
+					$("html").click(function(e) {
+						console.log($(e.target).parents("#views").length);
+						if(!$(e.target).parents(".menu-outer-h").length)
+							_self.$el.find(".menu-detail-h").hide();
+					});
+				},
+				
+				// show and hide dropdown
+				showMenuDropdown: function(e) {
+					var $target = $(e.target).next();
+					if($target.css('display') == "none") {
+						this.$el.find(".menu-detail-h").hide();
+						$target.show();
+					} else {
+						$target.hide();
+					}
 				},
 				
 				select : function(e) {
@@ -73,6 +94,10 @@ define(
 						$(selector).show();
 						$(selector).addClass('open');
 					}
+				},
+				
+				afterRender: function() {
+					this.hideAllDropdowns();
 				}
 
 			});
