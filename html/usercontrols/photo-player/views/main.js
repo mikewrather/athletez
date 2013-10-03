67a10fc0960df7700e4c08eda01a81e887ba0780
Mike Wrather <mike.wrather@gmail.com>
2013-10-03 15:31:19 -0700

    still styling image player

--- html/usercontrols/photo-player/views/main.js
+++ html/usercontrols/photo-player/views/main.js
@@ -4,7 +4,7 @@
  // Requires `define`, `require`
  // Returns {Photo Player View} constructor
  */
-define(['require', 'text!usercontrols/photo-player/templates/player.html', 'facade', 'views', 'utils', 'vendor', 'votes/models/vote' ], function(require, layoutTemplate) {
+define(['require', 'text!usercontrols/photo-player/templates/player.html','text!usercontrols/photo-player/templates/image-thumbs.html', 'facade', 'views', 'utils', 'vendor', 'votes/models/vote' ], function(require, layoutTemplate,imageThumbsTemplate) {
 
 	var self, facade = require('facade'), views = require('views'), SectionView = views.SectionView, 
 	utils = require('utils'), Channel = utils.lib.Channel, vendor = require('vendor'), 
@@ -12,7 +12,7 @@ define(['require', 'text!usercontrols/photo-player/templates/player.html', 'faca
 	//Models
 	PhotoPlayerView = SectionView.extend({
 		template : layoutTemplate,
-		thumbTemplate: '{{#data}}<li><a href="javascript: void(0);" class="thumb-link-h" data-index="{{_index}}" title="thumb"><img src="{{_thumbnail}}" alt="{{_thumbnail}}" /></a></li>{{/data}}',
+		thumbTemplate: imageThumbsTemplate,
 		events : {
 			'click .back-arrow-h' : 'backButton',
 			'click .next-arrow-h' : 'nextButton',
@@ -73,7 +73,7 @@ define(['require', 'text!usercontrols/photo-player/templates/player.html', 'faca
 		},
 		
 		initThumbsSection: function() {
-			var _self = this, dataLength = this.json.length, data = {};
+			var _self = this, dataLength = this.json.length, data = {},standard_thumb=null;
 			data.data = [];
 			console.error(this.json);
 			for(var i = 0; i < dataLength; i++) {
@@ -85,25 +85,83 @@ define(['require', 'text!usercontrols/photo-player/templates/player.html', 'faca
 				switch(mpay.enttypes_id) {
 					case '23':
 						//videos
-						extra._thumbnail = mpay.thumbs;
+
+						if(typeof(mpay.standard_thumb)=='object')
+						{
+							standard_thumb = mpay.standard_thumb;
+							extra._thumbnail = standard_thumb.url;
+						}
+						else
+						{
+							extra._thumbnail = mpay.thumbs;
+						}
+
 						extra._label = mpay.media.name;
 						extra._link = "javascript: void(0);";
 						if(mpay.media.hasOwnProperty('is_owner')) show_edit = mpay.media.is_owner;
 						break;
 					case '21':
 						//images
-						if ( typeof (mpay.types) == 'object' && mpay.types.small_thumb)
-							extra._thumbnail = mpay.types.standard_thumb.url;
+
 						extra._label = mpay.media_obj.name;
 						extra._link = "javascript: void(0);";
+
+						if ( typeof (mpay.types) == 'object')
+						{
+							//console.log(mpay.types);
+							if(typeof(mpay.types.standard_thumb)=='object')
+							{
+								standard_thumb = mpay.types.standard_thumb;
+								extra._thumbnail = standard_thumb.url;
+							}
+							else if(typeof(mpay.types.small_thumb)=='object')
+							{
+								standard_thumb = mpay.types.large_thumb;
+								extra._thumbnail = standard_thumb.url;
+							}
+							else if(typeof(mpay.types.large_thumb)=='object')
+							{
+								standard_thumb = mpay.types.large_thumb;
+								extra._thumbnail = standard_thumb.url;
+							}
+							else if(typeof(mpay.types.original)=='object')
+							{
+								//console.log(mpay.types.original);
+								standard_thumb = mpay.types['original'];
+								extra._thumbnail = standard_thumb.url;
+							}
+						}
+
 	
 						if(mpay.media_obj.hasOwnProperty('is_owner')) show_edit = mpay.media_obj.is_owner;
 	
 						break;
 					case '1':
 						//users
+
 						if ( typeof (mpay.user_picture_obj) == 'object')
-							extra._thumbnail = mpay.user_picture_obj.types.small_thumb.url;
+						{
+							if(typeof(mpay.user_picture_obj.types) == 'object')
+							{
+								if(typeof(mpay.user_picture_obj.types.standard_thumb)=='object')
+								{
+									standard_thumb = mpay.user_picture_obj.types.standard_thumb;
+									extra._thumbnail = standard_thumb.url;
+								}
+								else if(typeof(mpay.user_picture_obj.types.large_thumb)=='object')
+								{
+									standard_thumb = mpay.user_picture_obj.types.large_thumb;
+									extra._thumbnail = standard_thumb.url;
+								}
+								else if(typeof(mpay.user_picture_obj.types.original)=='object')
+								{
+									//console.log(mpay.types.original);
+									standard_thumb = mpay.user_picture_obj.types['original'];
+									extra._thumbnail = standard_thumb.url;
+								}
+							}
+						}
+
 						extra._label = mpay.label;
 						extra._sublabel = "Coming Soon";
 						extra._link = "/#profile/" + mpay.id;
@@ -114,8 +172,10 @@ define(['require', 'text!usercontrols/photo-player/templates/player.html', 'faca
 					case '8':
 						//games
 						extra._detailclass = "game";
-	
+
 						extra._thumbnail = mpay.game_picture!==null ? mpay.game_picture.types.small_thumb.url : "http://lorempixel.com/output/sports-q-c-440-440-4.jpg";
+						standard_thumb = mpay.game_picture!==null ? mpay.game_picture.types.small_thumb : {height:440,width:440,url:"http://cdn.athletez.com/resources/icons/game/square_game.png"}
+
 						extra._label = mpay.game_day;
 						extra._link = "/#game/" + mpay.id;
 						var team_str = "", teamLength = mpay.teams.length,
@@ -139,11 +199,62 @@ define(['require', 'text!usercontrols/photo-player/templates/player.html', 'faca
 						extra._sublabel = team_str;
 						break;
 				}
+
+
+				console.log("standard thumb",standard_thumb);
+
+				if(standard_thumb != "undefined" && standard_thumb != null)
+				{
+
+					var ratio_x = standard_thumb.width / 110,
+						ratio_y = standard_thumb.height / 110;
+
+					standard_thumb.width = parseInt(standard_thumb.width);
+					standard_thumb.height = parseInt(standard_thumb.height);
+
+					var overflow = (standard_thumb.width > standard_thumb.height) ?
+						-((standard_thumb.width / ratio_y) -110) / 2:
+						-((standard_thumb.height / ratio_x)-110) / 2;
+					if(overflow > 0) overflow = 0;
+
+					extra.styles = {};
+
+					if(standard_thumb.width > standard_thumb.height)
+						extra.styles = [
+							{
+								'property':'min-width',
+								'value':(standard_thumb.width / ratio_y) + 'px'
+							},
+							{
+								'property':'left',
+								'value':overflow + 'px'
+							}];
+					else
+						extra.styles =[
+							{
+								'property':'min-height',
+								'value':(standard_thumb.height / ratio_x) + 'px'
+							},
+							{
+								'property':'top',
+								'value':overflow + 'px'
+							}];
+				}
+
+
+
+				console.log(extra);
+
 				data.data.push(extra);
 			}
 			this.id = extra._id;
 			var markup = Mustache.to_html(this.thumbTemplate, data);
 			this.$el.find('.thumb-image-list-h').html(markup);
+
+
+
+
+
 		},
 		
 		
@@ -219,40 +330,62 @@ define(['require', 'text!usercontrols/photo-player/templates/player.html', 'faca
 
 			if(image_object != "undefined" && image_object != null)
 			{
-				var totalheight = parseInt(this.$el.height()) * .8,
-					image_height = parseInt(image_object.height) > 380 ? parseInt(image_object.height) : 380;
+				var self=this,loading_div = this.$el.find('div.loading_image');
+				loading_div.css('opacity','0');
 
-				console.log(totalheight,image_height,(totalheight-image_height)/2);
 
-				if(image_object.width > image_object.height || image_object.width == image_object.height)
-				{
-					var top = totalheight<image_height ? 10 : (totalheight-image_height)/2;
-					this.$el.find('img').css({
-						'max-width':'100%',
-						'top':top + 'px'
-					});
+
+
+
+				function showImage(event){
+					var self = event.data.self,
+						image_object = event.data.image_object,
+						totalheight = parseInt(self.$el.height()) * .8,
+						image_height = parseInt(image_object.height) >= 380 ? parseInt(image_object.height) : 380;
+
+					console.log(totalheight,image_height,(totalheight-image_height)/2);
+
+					console.log(event.data,self);
+
+					if(image_object.width > image_object.height || image_object.width == image_object.height)
+					{
+						var top = totalheight<image_height ? 10 : (totalheight-image_height)/2;
+						event.data.el.css({
+							'max-width':'100%',
+							'top':top + 'px'
+						});
+					}
+					else if(totalheight > image_height)
+					{
+						var top = (totalheight-image_height) / 2;
+						event.data.el.css({
+							'max-height':totalheight+'px',
+							'top':top+'px'
+						});
+					}
+					else
+					{
+						event.data.el.css({
+							'max-height':totalheight+'px',
+							'top':'0px'
+						});
+					}
+					event.data.el.css('opacity','100');
+					event.data.el.find('.large-image-h').off('load');
 				}
-				else
-				{
-					this.$el.find('img').css({
-						'max-height':'100%',
-						'top':'0px'
-
-					});
-				}
-
-
-				this.$el.find('.large-image-h').attr('src', image_object.url);
+				loading_div.find('.large-image-h').attr('src', image_object.url).on('load',{'el':loading_div,'self':this,'image_object':image_object},showImage);
 
 			}
-			
 
-			
 			this.$el.find('.thumb-image-list-h li').removeClass('selected-photo-thumb');
 			this.$el.find('.thumb-link-h[data-index='+this.index+']').parents('li').addClass('selected-photo-thumb');
 
 			console.log(extra);
 			routing.trigger('photo-player-section-reload', extra._enttypes_id, extra._id);
+		},
+
+		showImage: function($el){
+			$el.find('div.loading_image').css('opacity','100');
 		},
 
 		// **Method** `setOptions` - called by BaseView's initialize method
