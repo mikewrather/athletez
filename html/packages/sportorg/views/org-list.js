068e65ceee486e96337307108ad3f23562aeadcb
Sanjay Kumar <20sanjaykumar@gmail.com>
2013-10-04 20:23:42 +0530

    priority fixes

--- html/packages/sportorg/views/org-list.js
+++ html/packages/sportorg/views/org-list.js
@@ -28,6 +28,26 @@ function(facade,  views,   utils,   OrgItemView) {
 
         // Store constructor for the child views
         _view: OrgItemView,
+        
+        events: {
+        	'click .team-info-h': 'showinfo'
+        },
+        
+         showinfo: function(e) {
+        	e.preventDefault();
+        	if(!$(e.target).parents('.org-popup').length) {
+	        	
+	        	if($(e.target).find('.org-popup').hasClass('hide')) {
+		        	$('.org-popup').addClass('hide');
+		        	$(e.target).find('.org-popup').removeClass('hide');
+		        } else {
+		        	$('.org-popup').addClass('hide');
+		        }
+		        
+		        
+		        
+        	}
+        },
 
         initialize: function(options) {
             CollectionView.prototype.initialize.call(this, options);
