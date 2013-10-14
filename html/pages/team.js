ea027bbe1b45c64489cd5aa4e17af548c3b484b4
Sanjay Kumar <20sanjaykumar@gmail.com>
2013-10-14 20:48:07 +0530

    schedules not working on Mike branch

--- html/pages/team.js
+++ html/pages/team.js
@@ -41,7 +41,7 @@ define([
     "team/views/comment-list",
     "team/views/menu",
     "media/models/image",
-    //"schedules/views/schedule-list",
+    "schedules/views/schedule-list",
     "roster/views/roster",
     "profile/views/fans-image-list"
     
@@ -68,7 +68,7 @@ define([
 		TeamCommentOnList = require("team/collections/commentson"),
         TeamHeaderView = require("team/views/header"),
         TeamAddMediaView = require("team/views/add-media"),
-       // TeamOrgListView = require("schedules/views/schedule-list"),
+        TeamOrgListView = require("schedules/views/schedule-list"),
        // TeamScheduleListView = require("sportorg/views/schedule-list"),
         TeamCompetitorTeamListView = require("sportorg/views/competitorteam-list"),
         TeamVideoListView = require("team/views/video-list"),
