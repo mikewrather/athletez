2e10f94103d6a6a44f6ca67d1074677022b676f0
Mike Wrather <mike.wrather@gmail.com>
2013-10-14 13:17:19 -0700

    fb stuff and new build.

--- html/pages/signup/views/facebooksignup.js
+++ html/pages/signup/views/facebooksignup.js
@@ -48,8 +48,9 @@ function (
         Fbinit : function(callLogin){
             var current =this;
             window.fbAsyncInit = function() {
+	            console.log("facebook");
                 FB.init({
-                    appId      : '239430712864961', // App ID
+                    appId      : '219148511595084', // App ID
                     status     : true, // check login status
                     cookie     : true, // enable cookies to allow the server to access the session
                     xfbml      : true,  // parse XFBML
