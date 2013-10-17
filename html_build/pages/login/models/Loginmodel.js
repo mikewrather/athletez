6fef5c2bc08a87012121309b45c11b79d91325fa
Mike Wrather <mike.wrather@gmail.com>
2013-10-17 09:35:46 -0700

    new build

--- html_build/pages/login/models/Loginmodel.js
+++ html_build/pages/login/models/Loginmodel.js
@@ -1,1 +1,1 @@
-define(["models/base"],function(e){var t;return t=e.extend({defaults:{},url:"api/user/login",payload:{}}),t});
+define(["models/base"],function(e){var t;return t=e.extend({defaults:{},url:"api/user/login?time="+(new Date).getTime(),payload:{}}),t});
