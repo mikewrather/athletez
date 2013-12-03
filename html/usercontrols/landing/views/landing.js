ca13ac5f92189928a793ca6525e100dee82fb025
Mike Wrather <mike.wrather@gmail.com>
2013-12-03 00:34:38 -0800

    more work on landing

--- html/usercontrols/landing/views/landing.js
+++ html/usercontrols/landing/views/landing.js
@@ -26,13 +26,14 @@ define(['require',
 		layoutTemplate = require('text!usercontrols/landing/templates/landing.html');
 	//Models
 
-	return Backbone.View.extend({
+	return BaseView.extend({
 		// template for dropdown
 		template : layoutTemplate,
 		// set to true if requires multiple selection
 		/*Bind Events on controls present in current view template*/
 		events : {
-			"click .close":"closePopup"
+			"click #browse":"closePopup",
+			"click #sign-in":"signIn"
 		},
 		cssArr : ["usercontrols/landing/landing.css"],
 		/*initialize gets called by default when constructor is initialized*/
@@ -66,6 +67,7 @@ define(['require',
 			options.background_image = "http://cdn.athletez.com/resources/img/landing/" + bgs[rand % bgs.length];
 			console.error(options);
 			routing.trigger('common-popup-open', options);
+			this.$el = $('#landing .modal-body');
 
 			function onPop(){
 				Channel('popup-finished-launch-'+options.id).unsubscribe();
@@ -89,19 +91,22 @@ define(['require',
 			this.regModel = new signupBaseModel();
 
 
-			this.selectTypeView = new signupBaseView({
+			this.regView = new signupBaseView({
 				model : this.regModel,
 				name : "Select Registration Type",
 				destination : "#reg-landing",
 				openAsaPage: true,
 				showOnLanding:true
 			});
-			console.log(this.selectTypeView);
 
 		},
 
+		signIn: function(e){
+			this.regView.showLogin(e);
+		},
 		closePopup:function(){
-
+			console.log("close popup");
+			routing.trigger('common-popup-close');
 		}
 	});
 });
