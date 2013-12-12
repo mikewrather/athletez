16964a6b71909543086fe6e4302396180d7d2f65
Mike Wrather <mike.wrather@gmail.com>
2013-12-11 17:33:37 -0800

    point to s3 instead of cdn for cache issue

--- html/usercontrols/landing/views/landing.js
+++ html/usercontrols/landing/views/landing.js
@@ -64,7 +64,7 @@ define(['require',
 			var bgs = ['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg'];
 			console.log(rand,rand % bgs.length);
 
-			options.background_image = "http://cdn.athletez.com/resources/img/landing/" + bgs[rand % bgs.length];
+			options.background_image = "http://athletez.s3.amazonaws.com/resources/img/landing/" + bgs[rand % bgs.length];
 			console.error(options);
 			routing.trigger('common-popup-open', options);
 			this.$el = $('#landing .modal-body');
