2f3b74d0ef0ac59dcef9cb31e6de3364b80a1417
Sanjay Kumar <20sanjaykumar@gmail.com>
2013-12-21 11:01:39 +0530

    #1194

--- html/pages/profile/views/fans-image-list.js
+++ html/pages/profile/views/fans-image-list.js
@@ -5,16 +5,24 @@ define(['facade', 'utils', 'media/views/image-list'],
 function(facade,  utils,   BaseImageListView) {
 
     var FansImageListView;
-
     FansImageListView = BaseImageListView.extend({
-
 	    imagetype: 'large_thumb',
 		//name: "fans List",
-        setupAddView: function() {
-            
-        }
-
+        setupAddView: function() {},
+        addButtons: function() {
+			var _self = this;
+            setTimeout(function() {
+	            if(!$("#add-fans-icons").length) {
+	            	_self.$el.find(_self.listView).prepend('<li id="add-fans-icons"><a href="javascript: void(0);" class="add-to-fans-h"></a></li>');
+            	}
+            	var json = _self.collection.toJSON();
+		        	for(var i in json) {
+		        		if(routing.loggedInUserId == json[i].payload.id) {
+		        			_self.$el.find("#add-fans-icons").addClass("hide");
+		        		}
+		        	}
+            }, 0);
+		}
     });
-
     return FansImageListView;
 });
