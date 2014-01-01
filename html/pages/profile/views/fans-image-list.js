2c5ea2c1b974eed055814df2423307b3a0287593
Sanjay Kumar <20sanjaykumar@gmail.com>
2014-01-01 12:44:19 +0530

    #1199

--- html/pages/profile/views/fans-image-list.js
+++ html/pages/profile/views/fans-image-list.js
@@ -7,18 +7,25 @@ function(facade,  utils,   BaseImageListView) {
     var FansImageListView;
     FansImageListView = BaseImageListView.extend({
 	    imagetype: 'large_thumb',
-		//name: "fans List",
         setupAddView: function() {},
         addButtons: function() {
 			var _self = this;
             setTimeout(function() {
 	            if(!$("#add-fans-icons").length) {
-	            	_self.$el.find(_self.listView).prepend('<li id="add-fans-icons"><a href="javascript: void(0);" class="add-to-fans-h">Add to fans list</a></li>');
+	            	var html = '<li id="add-fans-icons" class="add-tile-outer">\
+					<div>\
+					<a href="javascript: void(0);" class="add-to-fans-h add-to-fans pull-left tiles"></a>\
+					<span class="hide">Add to fans list</span></div>\
+					<div>\
+					<span class="hide">Add to fans list</span>\
+					<a href="javascript: void(0);" class="fb-invite-tile-btn invite-to-fb-h tiles pull-right" title="Add to fb"></a></div>\
+					</li>';
+	            	_self.$el.find(_self.listView).prepend(html);
             	}
             	var json = _self.collection.toJSON();
 		        	for(var i in json) {
 		        		if(routing.loggedInUserId == json[i].payload.id) {
-		        			_self.$el.find("#add-fans-icons").addClass("hide");
+		        			_self.$el.find(".add-to-fans-h").addClass("link-disabled");
 		        		}
 		        	}
             }, 0);
