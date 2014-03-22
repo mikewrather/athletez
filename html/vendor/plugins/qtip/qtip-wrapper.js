2ab8d98fcdc4869c9f29b5fada4d6c1b6d0aa25a
Sanjay Kumar <20sanjaykumar@gmail.com>
2014-03-22 15:50:58 +0530

    #1368

--- html/vendor/plugins/qtip/qtip-wrapper.js
+++ html/vendor/plugins/qtip/qtip-wrapper.js
@@ -1,13 +1,14 @@
 define(['require', "vendor/plugins/qtip/qtip"], function() {
 		// jquery plugin for qtip wrapper	
-		jQuery.fn.qtip2 = function(options) {
-			if(!routing.mobile) {
-				$(this).qtip(options);
+		jQuery.fn.qtip2 = function(options, showOnMobile) {
+			if(!routing.mobile || showOnMobile) {
+				$(this).each(function(){
+          			$(this).qtip(options);
+        		});
 			}
 		};
-			
-		return function() {		
 
+		return function() {		
 		};
 });
 
