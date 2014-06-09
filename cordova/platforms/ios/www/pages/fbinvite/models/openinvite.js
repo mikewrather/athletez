8c6e511bd3139dbc19030d36ff77e37dbaf698d6
Mike Wrather <mike.wrather@gmail.com>
2014-06-09 10:59:58 -0700

    reinstalled the iOS platform and plugins

--- cordova/platforms/ios/www/pages/fbinvite/models/openinvite.js
+++ cordova/platforms/ios/www/pages/fbinvite/models/openinvite.js
@@ -1,1 +1,1 @@
-define(["models","facade"],function(e,t){var n=e.BaseModel,r=t.$,i=t._;return n.extend({url:function(){return base_url+"api/invite/openinvite/"}})});
+define(["models","facade"],function(e,t){var n=e.BaseModel,r=t.$,i=t._;return n.extend({url:function(){return"/api/invite/openinvite/"}})});
